523063acd8ed077e2dc84bf720d0e89f0a9f2ccb
basti <basti@boerakker.com>
2018-08-17 23:49:27 +0200

    add postman ENV Var

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -1,7 +1,9 @@
 export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:$HOME/.local/bin
 
 export GOPATH=$HOME/go
 export NVM_DIR="$HOME/.nvm"
+export POSTMAN_DISABLE_GPU=true
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
