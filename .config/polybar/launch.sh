a0e47ac6572c3f7c9d9b005c355d186a582e49db
Sebastian Boerakker <basti@boerakker.com>
2018-12-10 09:49:18 +0100

    add primary screen to launcher

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar on each Monitor
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  for m in $(xrandr --query | grep " connected primary" | cut -d" " -f1); do
     MONITOR=$m polybar --reload top &
     MONITOR=$m polybar --reload bottom &
   done
