54825ab6ca8b334a7aaf2ac35fa937078d873764
Sebastian Boerakker <basti@boerakker.com>
2019-01-07 10:47:13 +0100

    udpate query

--- .config/polybar/scripts/update-notifier.sh
+++ .config/polybar/scripts/update-notifier.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-query=`yay -Qu`
+query=$(yay -Qu)
 if [[ $query == *"Get https://aur.archlinux.org"* ]]; then
 	echo "-"
 else
 	if [[ -z "$query"  ]]; then
 		echo "0"
 	else
-		echo $query | wc -l
+		yay -Qu | wc -l
 	fi
-#	yay -Qu | wc -l
 fi
