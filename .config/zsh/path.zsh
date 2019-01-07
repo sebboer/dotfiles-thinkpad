334ba5c5cf708445173ddbecf5987fbd27328512
Sebastian Boerakker <basti@boerakker.com>
2019-01-07 10:44:37 +0100

    add android paths

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -7,3 +7,7 @@ export POSTMAN_DISABLE_GPU=true
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
+
+# Android
+export ANDROID_SDK_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_SDK_HOME/emulator
