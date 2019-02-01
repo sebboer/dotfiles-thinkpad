1426e56f89c492dcfac3774c55f89839e34168cc
Sebastian Boerakker <basti@boerakker.com>
2019-02-01 14:12:50 +0100

    shorten script

--- .config/polybar/scripts/update-notifier.sh
+++ .config/polybar/scripts/update-notifier.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-sleep 10
-query=$(yay -Qu)
-if [[ $query == *"Get https://aur.archlinux.org"* ]]; then
-	echo "-"
-else
-	if [[ -z "$query"  ]]; then
-		echo "0"
-	else
-		yay -Qu | wc -l
-	fi
-fi
+echo $(yay -Qu 2> /dev/null | wc -l)
