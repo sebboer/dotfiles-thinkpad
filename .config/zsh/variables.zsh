2cbc1c0af5d13ee76ca98be461682f3a67645f46
Sebastian Boerakker <basti@boerakker.com>
2019-02-25 16:20:54 +0100

    add fzf env var

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -4,5 +4,6 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export TERM="xterm-256color"
 export XDG_CONFIG_HOME=$HOME/.config/
+export FZF_DEFAULT_COMMAND="ag --hidden --ignore .git -l -g ''"
 
 
