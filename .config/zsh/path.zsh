31596c9a4dd4e89b946023164c400ec2e02a28d5
Sebastian Boerakker <basti@boerakker.com>
2019-03-05 00:03:08 +0100

    cleanup

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -2,11 +2,7 @@ export PATH=$PATH:$HOME/.cargo/bin
 export PATH=$PATH:$HOME/.local/bin
 
 export GOPATH=$HOME/go
-export NVM_DIR="$HOME/.nvm"
 export POSTMAN_DISABLE_GPU=true
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
 
 # Android
 export ANDROID_SDK_HOME=$HOME/Android/Sdk
@@ -16,4 +12,4 @@ export PATH=$PATH:$ANDROID_SDK_HOME/emulator
 export PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin
 
 # NVM
-export NVM_DIR="~/.config/.nvm"
+export NVM_DIR="$HOME/.config/.nvm"
