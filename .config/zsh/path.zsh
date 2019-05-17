6dfc09c3ef467da776a2f69c7656e8d73dae76a0
Sebastian Boerakker <basti@boerakker.com>
2019-05-17 13:40:25 +0200

    rm rubypath && disable nvm path

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -8,8 +8,5 @@ export POSTMAN_DISABLE_GPU=true
 export ANDROID_SDK_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_SDK_HOME/emulator
 
-# Ruby
-export PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin
-
 # NVM
-export NVM_DIR="$HOME/.config/.nvm"
+#export NVM_DIR="$HOME/.config/.nvm"
