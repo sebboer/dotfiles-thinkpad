4e70fd924bf4fb03959bbc43e886480f2f0eeff1
Sebastian Boerakker <basti@boerakker.com>
2019-10-21 12:28:46 +0200

    remove unused aliases

--- .config/zsh/common-aliases.zsh
+++ .config/zsh/common-aliases.zsh
@@ -3,14 +3,6 @@
 alias zshconfig="mate ~/.zshrc"
 alias ohmyzsh="mate ~/.oh-my-zsh"
 
-#XRANDR
-alias workspace-xrandr="xrandr --output DP2-2 --primary --output eDP1 --off"
-
-# Pywal
-alias wal-wood="wal -q -n -a "92" -i ~/Pictures/Wallpapers/wood.jpg"
-alias wal-sunset="wal -q -n -a "92" -i ~/Pictures/Wallpapers/sunset.jpg"
-
-
 # Exa 
 alias ls="exa"
 alias la="exa -lah"
@@ -26,8 +18,6 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 # i3
 alias i3config='vim /home/basti/.config/i3/config'
 alias lock='/usr/bin/betterlockscreen -l'
-alias sound='alsamixer'
-alias polyconfig='vim ~/.config/polybar/config'
 
 # Docker
 alias dk='docker'
@@ -41,20 +31,9 @@ alias dkstart='systemctl start docker'
 alias dkco='docker-compose'
 alias dkstopall='docker stop $(docker ps -a -q)'
 
-# Calendar
-alias cal='gcalcli'
-alias calp='gcalcli --calendar=sebastian.boerakker@gmail.com'
-alias calg='gcalcli --calendar="Gemeinsamer Kalender"'
-alias calm='gcalcli --calendar="Michelle"'
-alias calw='gcalcli calw'
-
 #
 alias vim='nvim'
-alias grive='grive -p /home/basti/googledrive/'
-alias gnome-ctl='env XDG_CURRENT_DESKTOP=GNOME gnome-control-center'
-alias gnucash='GKT_THEME=Adwaita LANGUAGE=de_DE.UTF-8 LANG=de_DE.UTF-8 LC_ALL=de_DE.UTF-8 gnucash'
 alias xclipsel='xclip -selection "clipboard"'
-alias shebang-bash='echo "#!/bin/bash" | xclipsel'
 alias al='la'
 alias du='du -a -h --max-depth=1 | sort -n'
 alias cd..='cd ..'
