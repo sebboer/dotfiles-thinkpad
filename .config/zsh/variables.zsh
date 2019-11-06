a94024506ef304b532e1da36ff4c2edf5b99814d
Sebastian Boerakker <basti@boerakker.com>
2019-11-06 07:52:11 +0100

    update nnn variables

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -7,7 +7,7 @@ export XDG_CONFIG_HOME=$HOME/.config/
 export FZF_DEFAULT_COMMAND="ag --hidden --ignore .git -l -g ''"
 
 # NNN
-export NNN_USE_EDITOR=1
+export NNN_USE_EDITOR=0
 export NNN_CONTEXT_COLORS='3254'
 export NNN_COPIER=$HOME/.config/nnn/copier.sh
 export NNN_BMS='d:~/Downloads;g:~/googledrive;m:~/mnt;s:~/googledrive/Studium;w:~/workspace;v:~/Videos;p:~/Pictures;'
